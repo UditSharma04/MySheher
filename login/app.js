f39469eff998092bd2b5963fbe42495383189b74
Udit Sharma <109049436+UditSharma04@users.noreply.github.com>
2024-02-08 13:39:09 +0530

    add night mode , remove extra links

--- login/app.js
+++ login/app.js
@@ -1,6 +1,6 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
+const container = document.querySelector(".main-container");
 
 sign_up_btn.addEventListener('click', () =>{
     container.classList.add("sign-up-mode");
@@ -10,6 +10,5 @@ sign_in_btn.addEventListener('click', () =>{
     container.classList.remove("sign-up-mode");
 });
 function redirectToIndexPage() {
-    // Redirect to index.html
     window.location.href = '../index.html';
 }
